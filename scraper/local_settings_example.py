8261bb6f13a7a290f713cbbc346a71c461729e9c
jmlin <jmlin@jmlinteki-MacBook-Air.local>
2014-04-06 17:31:54 +0800

    fix sentry url

--- scraper/local_settings_example.py
+++ scraper/local_settings_example.py
@@ -1,6 +1,6 @@
 
 # Sentry DSN
-SENTRY_DSN = "http://70010b884a9e4ae7a8ad3e2c29b5da33:d91efd8da9d74f55a9923ac93c6be811@localhost:9000/2"
+SENTRY_DSN = "http://70010b884a9e4ae7a8ad3e2c29b5da33:d91efd8da9d74f55a9923ac93c6be811@10.8.0.1:9000/2"
 
 
 # Redis Queue
